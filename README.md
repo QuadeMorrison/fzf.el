e87fcac4cce10c8757c38da3e1ccc934335ade6b
Quade Morrison <Quade@morrison.local>
2017-04-12 21:35:36 -0700

    Restructing code to allow users to supply sources to fzf

--- README.md
+++ README.md
@@ -8,9 +8,58 @@ An Emacs front-end for [fzf][1].
 
 fzf.el can be installed through [MELPA][2].
 
+# compatibility
+While this package does work with the newest version of FZF, the emulator displays the text much
+slower than older versions. Unless there is a newer feature that you require it is recommended that
+you use an older version. Version 0.15.6 has been tested and displays results quickly.
+
 # usage
 
+There are several built in functions in fzf.el, such as the following which will let you search for
+files in your working directory, or if projectile is installed, fzf will instead search for files
+in the projectile directory, if a projectile project is found.
+
 `M-x fzf`
+
+It is simple to provide your own source to FZF. FZF can take bash commands piped in like
+`find ~/ type -d | fzf`. This allows you to use FZF to search through the directories
+under your `~/` directory. So the following implementation would let you search through your
+directories and print the results
+
+```cl
+(defun fzf-print-directory ()
+  (fzf/start
+   "find ~ -type d"
+   "   FZF Search Directories"
+   '(lambda (result current-dir) (print result))))
+```
+
+`(defun fzf-projectile-projects ()
+  "Search through the known projectile projects, and the search for a file from
+   the selected project to switch to"
+  (fzf/start
+   (fzf/list-to-fzf-results (sort (projectile-load-known-projects) 'string-lessp))
+   "   FZF Projectile Projects"
+   '(lambda (project-dir current-dir) (when (file-exists-p project-dir)
+                                        (fzf-directory project-dir)))))`
+
+`(defun fzf-buffers ()
+  "Search currently open buffers, ignoring things like mini-buffers and helm-buffers"
+  (let ((buffer-list
+         (cl-remove-if
+          '(lambda (str)
+             (or (string-match "^\s[*]" str)
+                 (string-match "^[*]helm" str)))
+          (map 'list 'buffer-name (buffer-list)))))
+    (fzf/start
+     (fzf/list-to-fzf-results buffer-list)
+     "   FZF Buffers"
+     '(lambda (buffer current-dir)
+        (when (get-buffer buffer)
+          (switch-to-buffer buffer))))))`
+
+For more details on how this works, read the documentation found in the doc-string of the fzf/start
+function.
 
 # license
 
--- fzf.el
+++ fzf.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2015 by Bailey Ling
 ;; Author: Bailey Ling
 ;; URL: https://github.com/bling/fzf.el
+;; Package-Version: 20161226.936
 ;; Filename: fzf.el
 ;; Description: A front-end for fzf
 ;; Created: 2015-09-18
@@ -37,7 +38,10 @@
 ;;
 ;; M-x fzf
 ;; M-x fzf-directory
-;;
+;; M-x fzf-projectile-projects
+;; M-x fzf-cd
+;; M-x fzf-dir-find-file
+;; M-x fzf-themes
 ;;; Code:
 
 (defgroup fzf nil
@@ -54,8 +58,12 @@
   :type 'string
   :group 'fzf)
 
-(defcustom fzf/args "-x --color bw --margin 1,0"
-  "Additional arguments to pass into fzf."
+(defcustom fzf/global-args "-x"
+  "Additional arguments to pass into all instances of fzf.
+   Example arguments include:
+       --margin 1,0
+       --color=bw
+   Too see more options, find fzf in your terminal and type fzf -h"
   :type 'string
   :group 'fzf)
 
@@ -64,59 +72,166 @@
   :type 'bool
   :group 'fzf)
 
-(defun fzf/after-term-handle-exit (process-name msg)
-  (let* ((text (buffer-substring-no-properties (point-min) (point-max)))
-         (lines (split-string text "\n" t "\s.*\s"))
-         (target (car (last (butlast lines 1))))
-         (file (expand-file-name target)))
-    (kill-buffer "*fzf*")
-    (jump-to-register :fzf-windows)
-    (when (file-exists-p file)
-      (find-file file)))
-  (advice-remove 'term-handle-exit #'fzf/after-term-handle-exit))
+(defconst fzf/buffer-name "*fzf*"
+  "The name of the buffer that the fzf process runs in")
 
-(defun fzf/start (directory)
+(defun fzf/after-term-handle-exit (process-name msg)
+  "Process result from FZF, once it closes, and send the result to the
+   user defined exit handler"
+  (advice-remove 'term-handle-exit #'fzf/after-term-handle-exit)
+  (let* ((text (buffer-substring-no-properties (point-min) (point-max)))
+         (lines (split-string text "\n"))
+         (target (nth (- (length lines) 4) lines))
+         (current-dir default-directory))
+    (jump-to-register :fzf-windows)
+    (kill-buffer fzf/buffer-name)
+    (when on-term-exit (funcall on-term-exit target current-dir))))
+
+(defun fzf/create-buffer ()
+  "Create the bottom split where FZF will go"
+  (let ((height (if fzf/position-bottom (- fzf/window-height) fzf/window-height)))
+   (get-buffer-create fzf/buffer-name)
+   (split-window-vertically height)
+   (when fzf/position-bottom (other-window 1))
+   (switch-to-buffer fzf/buffer-name)))
+
+(defun fzf/disable-problem-settings ()
+  "Various settings that are known to cause artifacts, see #1 for more details."
+  (linum-mode 0)
+  (setq-local scroll-margin 0)
+  (setq-local scroll-conservatively 0)
+  (setq-local term-suppress-hard-newline t) ;for paths wider than the window
+  (face-remap-add-relative 'mode-line '(:box nil)))
+
+(defun fzf/start-term (&optional args)
+  "Starts the term emulator where FZF is run"
   (require 'term)
+  (let* ((args-list (concat fzf/global-args args))
+         (fzf-with-args (concat fzf/executable " --print-query " args-list))
+         (command (if source (concat source " | " fzf-with-args) fzf-with-args)))
+    (make-term "fzf" "command" nil "eval" command)
+    (term-char-mode)
+    (setq mode-line-format mode-line-string)))
+
+(defun fzf/start (source
+                  mode-line-string term-exit-handler
+                  &optional do-before
+                  &optional args)
+  "Starts the FZF process. FZF can be used to search anything as long as you
+   provide the source by piping its STDOUT as the STDIN to FZF. fzf.el handles
+   piping the source into FZF for you. For an example of the use of this function
+   look at any of the built in implementations, such as fzf.
+
+   source: A bash command, for instance `find ~/ type -f`
+   Note that some commands might not behave as expected. If you have a list
+
+   fzf/list-to-fzf-results function to generate the source.
+
+   mode-line-string: The string that appears at the bottom of FZF to tell you
+   what FZF is searching
+
+   term-exit-handler: A function that takes the [results current-dir] arguments.
+   This function lets you take the results from FZF and do something with them.
+
+   do-before: A function that lets you specify anything that needs to happen
+   before FZF is run.
+
+   args: Additional arguments to be passed into fzf"
+  (setq on-term-exit term-exit-handler)
   (window-configuration-to-register :fzf-windows)
   (advice-add 'term-handle-exit :after #'fzf/after-term-handle-exit)
-  (let ((buf (get-buffer-create "*fzf*"))
-        (window-height (if fzf/position-bottom (- fzf/window-height) fzf/window-height)))
-    (with-current-buffer buf
-      (setq default-directory directory))
-    (split-window-vertically window-height)
-    (when fzf/position-bottom (other-window 1))
-    (if fzf/args
-        (apply 'make-term "fzf" fzf/executable nil (split-string fzf/args " "))
-      (make-term "fzf" fzf/executable))
-    (switch-to-buffer buf)
-    (linum-mode 0)
-    (set-window-margins nil 1)
 
-    ;; disable various settings known to cause artifacts, see #1 for more details
-    (setq-local scroll-margin 0)
-    (setq-local scroll-conservatively 0)
-    (setq-local term-suppress-hard-newline t) ;for paths wider than the window
-    (face-remap-add-relative 'mode-line '(:box nil))
+  (fzf/create-buffer)
+  (when do-before (funcall do-before))
+  (fzf/start-term args)
+  (fzf/disable-problem-settings))
 
-    (term-char-mode)
-    (setq mode-line-format (format "   FZF  %s" directory))))
+(defun fzf/get-directory (directory)
+  "Returns the directory that should be used. If a directory is provided,
+   just return that, else if directory is nil check for a projectile directory,
+   otherwise return the current buffers directory"
+  (if directory directory
+    (if (fboundp #'projectile-project-root)
+        (condition-case err
+            (projectile-project-root)
+          (error default-directory))
+      default-directory)))
+
+(defun fzf/list-to-fzf-results (list)
+  "Generates a bash script that can be used as a source from a list"
+  (let* ((delimiter ",")
+         (list-as-string (s-join delimiter list)))
+    (format "IFS='%s' read -a array <<< '%s';
+            delimiter='\\n';
+            regex=\"$(printf \"%%s${delimiter}\" \"${array[@]}\" )\";
+            echo \"$regex\""
+            delimiter
+            list-as-string)))
 
 ;;;###autoload
-(defun fzf ()
-  "Starts a fzf session."
+(defun fzf (&optional directory &optional do-before)
+  "Search for a file and then switch to that file in the current buffer"
   (interactive)
-  (if (fboundp #'projectile-project-root)
-      (fzf/start (condition-case err
-                     (projectile-project-root)
-                   (error
-                    default-directory)))
-    (fzf/start default-directory)))
+  (fzf/start
+   nil
+   (format "   FZF  %s" (fzf/get-directory directory))
+   '(lambda (file current-dir)
+      (let ((file-path (expand-file-name file current-dir)))
+        (message file-path)
+        (when (file-exists-p file-path) (find-file file-path))))
+   do-before))
 
 ;;;###autoload
 (defun fzf-directory (directory)
-  "Starts a fzf session at the specified directory."
-  (interactive "D")
-  (fzf/start directory))
+  "Search for a file in the supplied directory and then switch to that
+   file in the current directory"
+  (interactive)
+  (fzf directory '(lambda () (cd directory))))
+
+;;;###autoload
+(defun fzf/find-dir (term-exit-handler &optional directory)
+  "Boiler plate function for FZF commands that use FZF to first search
+   directories"
+  (let ((dir (substring (fzf/get-directory directory) 0 -1)))
+    (fzf/start
+     (format "find %s -type d" dir)
+     (format "   FZF  %s" dir)
+     term-exit-handler)))
+
+;;;###autoload
+(defun fzf-cd (&optional directory)
+  "Search for a directory and then change the default directory to the
+   found directory"
+  (interactive)
+  (fzf/find-dir
+   '(lambda (new-dir current-dir)
+      (when (file-exists-p new-dir)
+        (cd new-dir)
+        (message (format "Working directory set to %s" new-dir))))
+   directory))
+
+;;;###autoload
+(defun fzf-dir-find-file (&optional directory)
+  "Search for a directory, then search for a file in that directory, and
+   then switch to that file in the current buffer"
+  (interactive)
+  (fzf/find-dir
+   '(lambda (new-dir current-dir)
+      (when (file-exists-p new-dir)
+        (fzf--directory new-dir)))
+   directory))
+
+;;;###autoload
+(defun fzf-themes()
+  "Search through all installed themes and then switch to them. Note some themes
+   may have result in issues with FZF upon switching, such as the base16 themes,
+   though your experience may vary. (If you use a base16 theme as a default theme,
+   you shouldn't have any issues. The problem only happens when you switch using
+   this method)"
+  (fzf/start
+   (fzf/list-to-fzf-results (map 'list 'symbol-name (custom-available-themes)))
+   "   FZF Themes"
+   '(lambda (theme) current-dir (load-theme (intern theme)))))
 
 (provide 'fzf)
 ;;; fzf.el ends here
